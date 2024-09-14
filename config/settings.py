bf51323139197379b54e1b2e13174fb41fe7cf39
pandichef <rashtyda@gmail.com>
2024-09-13 20:54:24 -0500

    add login_required

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware"  # for internationalization
     # "base_app.check_job_status.CheckJobStatus",
 ]
 
